2396ce9f666ab7f790a0c7df014e86a5bbcee54e
Нарышкин Андрей <narishkin_a_a@mail.ru>
2022-04-17 20:04:07 +0300

    add info page

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,19 +1,34 @@
 <template>
   <div class="header">
-    <h2>ANTI WORDLE (RU)</h2>
-
+    <h2><img id="info" src="@/images/info.png" :onclick="showInfo"/> ANTI WORDLE (RU)</h2>
+    <InfoComponent v-if="isShow" v-on:close-info="closeInfo" ref="childComponent"/>
 <!--    <br>-->
   </div>
   <GameView ref="childComponent"/>
 </template>
 <script>
 import GameView from './components/Game.vue';
+import InfoComponent from "@/components/Info";
 
 
 export default {
   name: 'app',
+  data: function (){
+    return {
+      isShow: false
+    }
+  },
   components: {
+    InfoComponent,
     GameView
+  },
+  methods: {
+    showInfo: function (){
+      this.isShow = true
+    },
+    closeInfo: function (){
+      this.isShow = false
+    }
   }
 }
 </script>
@@ -28,6 +43,11 @@ export default {
   height: calc(100% - 16px);
 }
 
+#info{
+  vertical-align: middle;
+  width: 28px;
+  height: 28px;
+}
 .header {
   margin-right: 60px;
 }
---
+++ ui/src/components/Info.vue
@@ -0,0 +1,45 @@
+<template>
+
+  <div class="infoRoot">
+    <h3> Правила <button id="closeButton" src="@/images/info.png" v-on:click="close"> X </button></h3>
+    <div> Загадайте слово (например РЕЗКА)</div>
+    <br/>
+    <div> Нажимайте на квадраты в соответствии с правилами WORDLE</div>
+    <div> для предложенного слова</div>
+    <img id="word" src="@/images/word.png">
+    <div>Обращайте внимание если в слове несколько одинаковых букв!</div>
+
+  </div>
+
+</template>
+<script>
+
+export default {
+  name: 'InfoComponent',
+  methods: {
+    close: function () {
+      this.$emit("close-info");
+    }
+  }
+}
+</script>
+<style>
+.infoRoot {
+  position: absolute;
+  left: 30%;
+  top:70px;
+  width: 40%;
+  height: calc(90% - 70px);
+  background-color: lightblue;
+}
+#closeButton {
+  float: right;
+  margin-right: 10px;
+}
+#word {
+  margin-top: 20px;
+  margin-bottom: 20px;
+  width: 340px;
+  height: 95px;
+}
+</style>
